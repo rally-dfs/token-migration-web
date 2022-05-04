da2c6626815a0855ce47eeb2e72b6187cd1bae4c
Ryan Weald <ryan@weald.com>
2022-05-04 13:56:04 -0700

    Get all solana web3 deps installed
    This requires quite a big shift in the way the project is setup as we
    need to move back to use React 17 rather than 18 as well as move to
    react-app-rewired to resolve webpack 5 not bundling node library issues.

--- src/components/connect_wallet_prompt.tsx
+++ src/components/connect_wallet_prompt.tsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import React, { useMemo } from 'react';
 
 const ConnectWalletPrompt = () => {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const solanaConnection = useMemo(() => clusterApiUrl(network), [network]);
+
+  const supportedWallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
   return (
-    <div>
-      <h3>To Get Started Connect a Wallet containing RLY V2 Tokens</h3>
-      <button>Connect Wallet</button>
-    </div>
+    <ConnectionProvider endpoint={solanaConnection}>
+      <WalletProvider wallets={supportedWallets} autoConnect>
+        <WalletModalProvider>
+          <div>
+            <h3>To Get Started Connect a Wallet containing RLY V2 Tokens</h3>
+            <button>Connect Wallet</button>
+          </div>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 };
 
