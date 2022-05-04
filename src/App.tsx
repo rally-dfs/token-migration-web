abc8736f84957d65c0b945d123235c8bcc687dc9
Ryan Weald <ryan@weald.com>
2022-05-04 15:06:11 -0700

    Cleanup importing/using of solana UI provided CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import Card from './components/card';
 import ConnectWalletPrompt from './components/connect_wallet_prompt';
 import SolanaContext from './components/solana_context';
-
-// Hack to use require because of CSS conflicts in solana provided UI library
-// TODO: see if we can fix this using require or import in the CSS directly
-require('./App.css');
-require('@solana/wallet-adapter-react-ui/styles.css');
+import './App.css';
 
 function App() {
   return (
