f655611b5eb0afc379d818f9dadb8a1bd4578edc
Ryan Weald <ryan@weald.com>
2022-05-11 10:32:48 -0700

    Link new token address in header to solscan

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import TransferHomePage from './pages/transfer_home_page';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import TransferV2ToV3Page from './pages/transfer_v2_to_v3_page';
 import TransferWormholePage from './pages/transfer_wormhole_page';
+import ButtonStyle from './styles/button.module.css';
 
 function App() {
   return (
@@ -13,8 +14,12 @@ function App() {
         <Header />
         <div style={{ marginBottom: 12 }}>
           <h1>Swap for new sRLY</h1>
-          <div style={{ fontSize: 14 }}>
-            sRLY3migNrkC1HLgqotpvi66qGkdNedqPZ9TJpAQhyh
+          <div style={{ fontSize: 14, marginTop: 8 }}>
+            <a
+              className={ButtonStyle.rly_link_button}
+              href="https://solscan.io/account/sRLY3migNrkC1HLgqotpvi66qGkdNedqPZ9TJpAQhyh">
+              sRLY3migNrkC1HLgqotpvi66qGkdNedqPZ9TJpAQhyh
+            </a>
           </div>
         </div>
         <SolanaContext>
